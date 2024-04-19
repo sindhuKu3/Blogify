08814d6887399bde886e8262e73a2696311da0b8
sindhuKu3 <sindhuku3@gmail.com>
2024-04-19 12:52:36 +0530

    commit changes

--- services/authentication.js
+++ services/authentication.js
@@ -1,5 +1,5 @@
 const JWT = require("jsonwebtoken") ; 
-const secret = "$uperman@123" ; 
+// const secret = process.env.JWT_SECRET ; 
  function createTokenForUser(user){
     const payload={
         _id : user._id ,
@@ -7,12 +7,12 @@ const secret = "$uperman@123" ;
         profileImageURL:user.profileImageURL ,
         role:user.role 
     }
-    const token = JWT.sign(payload, secret); 
+    const token = JWT.sign(payload, process.env.JWT_SECRET); 
     return token ; 
 }
 
 function validateToken(token){
-    const payload = JWT.verify(token,secret) ; 
+    const payload = JWT.verify(token, process.env.JWT_SECRET); ; 
     return payload ; 
 }
 
