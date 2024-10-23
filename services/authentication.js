acadf8f46d402070a074ce67cb41fbd26b3dd26d
sindhuKu3 <sindhuku3@gmail.com>
2024-10-24 01:51:03 +0530

    changes

--- services/authentication.js
+++ services/authentication.js
@@ -1,5 +1,5 @@
 const JWT = require("jsonwebtoken") ; 
-const secret = "$uperman@123" ; 
+const secret = "$uperman@123"; ; 
  function createTokenForUser(user){
     const payload={
         _id : user._id ,
@@ -7,7 +7,7 @@ const secret = "$uperman@123" ;
         profileImageURL:user.profileImageURL ,
         role:user.role 
     }
-    const token = JWT.sign(payload, secret); 
+    const token = JWT.sign(payload,secret); 
     return token ; 
 }
 
